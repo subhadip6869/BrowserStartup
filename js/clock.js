9ff903ceb817b39a21831ee31e26618e5ceedda1
Subhadip Dutta <subhadipdutta3000@gmail.com>
2023-06-16 10:56:35 +0530

    date added

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,21 @@
-let clockDiv = document.getElementById("clock");
-let weekdayDiv = document.getElementById("weekday");
-var timer = setInterval(clockTiming, 1000);
+const clockDiv = document.getElementById("clock");
+const weekdayDiv = document.getElementById("weekday");
+const month = document.getElementById("month");
+const day = document.getElementById("date");
 
-let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday']
+const timer = setInterval(clockTiming, 1000);
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday']
+const months = ["January", "February", "March", "April", "May", "June", 
+                "July","August","September","October","November","December"];
 
 function clockTiming() {
     var date = new Date();
     
-    weekdayDiv.innerHTML = `${weekdays[date.getDay()]}`
+    weekdayDiv.innerHTML = `${weekdays[date.getDay()]}`;
     // clockDiv.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    clockDiv.innerHTML = `${date.toLocaleTimeString()}`
+    clockDiv.innerHTML = `${date.toLocaleTimeString()}`;
+    month.innerHTML = `${months[date.getMonth()]}`;
+    day.innerHTML = `${date.getDate()}`
 }
 
 clockTiming();
